3304e62821ebba74fa1e939d0d7d58a4575c654d
anichucai <acai@fi.uba.ar>
2021-03-30 09:59:30 -0300

    Add junit config

---
+++ bin/README.md
@@ -0,0 +1,3 @@
+# Azure Pipelines - Java with Maeven
+
+[![Build Status](https://anamc.visualstudio.com/azure-pipelines-java/_apis/build/status/anichucai.azure-pipelines-java-2?branchName=master)](https://anamc.visualstudio.com/azure-pipelines-java/_build/latest?definitionId=3&branchName=master)
---
+++ src/test/Test_1.java
@@ -0,0 +1,17 @@
+package test;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class Test_1 {
+    @Test
+    public void test_method_1() {
+        assertTrue("This will succeed.", true);
+    }
+
+    @Test
+    public void test_method_2() {
+        assertTrue("This will succeed.", true);
+    }
+}
